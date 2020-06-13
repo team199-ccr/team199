ba999e0b3cd7a388d8059a064b4222149f84d1f4
Luiz <lf1405@hotmail.com>
2020-06-13 20:57:05 -0300

    Homescreen and public folder finalized

--- src/app.js
+++ src/app.js
@@ -5,20 +5,15 @@ import {
   Route
 } from "react-router-dom";
 
-import {Header} from './components'
-
 import {Homepage} from './pages'
 
 function App() {
   return (
     <Router>
-      <Header/>
       <Switch>
-
         <Route exact path="/">
           <Homepage/>
         </Route>
-
         <Route path="/">
           404 - FEZ MERDA AE GORDÃO, NÃO SEI ONDE TU QUERIA CHEGAR
         </Route>
